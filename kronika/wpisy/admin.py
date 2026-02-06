c09f4b5e71951480b5d89e3f67605ceb4cce85ad
Wiolka <sativa7@gmail.com>
2026-02-06 23:21:51 +0100

    DodanieDodanie modeli kroniki, migracji, konfiguracji admina oraz debug toolbara

--- kronika/wpisy/admin.py
+++ kronika/wpisy/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Person, Place, Entry, Photo
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Person)
+admin.site.register(Place)
+#admin.site.register(Entry)
+admin.site.register(Photo)
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ("title", "event_date", "category", "place")
+    list_filter = ("category", "event_date")
+    search_fields = ("title", "content")
+    filter_horizontal = ("persons",)
--- kronika/wpisy/models.py
+++ kronika/wpisy/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta: #zmieniamy liczbę mnogą
+        verbose_name = "Category"
+        verbose_name_plural = "Categories" 
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    relation = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=150, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.city})"
+
+
+class Entry(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    event_date = models.DateField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    place = models.ForeignKey(Place, on_delete=models.SET_NULL, null=True, blank=True)
+    persons = models.ManyToManyField(Person, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Entry"
+        verbose_name_plural = "Entries"
+
+class Photo(models.Model):
+    entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200, blank=True)
